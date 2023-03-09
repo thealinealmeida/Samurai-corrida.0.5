06c8757f8af3b451fb32ca19a60c4f7fb7106137
thealinealmeida <68761501+thealinealmeida@users.noreply.github.com>
2023-03-09 18:10:58 -0300

    0.5

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var canvas;
+var backgroundImage, car1_img, car2_img, track;
+var fuelImage, powerCoinImage, lifeImage;
+var obstacle1Image, obstacle2Image;
+var database, gameState;
+var blastImage;
+var form, player, playerCount;
+var allPlayers, car1, car2, fuels, powerCoins, obstacles;
+var cars = [];
+
+function preload() {
+  
+  
+}
+
+function setup() {
+  
+}
+
+function draw() {
+  
+  
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
